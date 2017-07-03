a5dc3aec63ffce98952e11b4d4da42bd459eda76
RumiRajbhandari <rumirajbhandari@gmail.com>
2017-07-03 11:12:59 +0545

    First commit with osm basic

---
+++ app/src/main/java/com/example/root/openstreetmapdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.root.openstreetmapdemo;
+
+import android.os.StrictMode;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        GeoPoint startPoint = new GeoPoint(27.7089603, 85.3261328);
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        mapController.setCenter(startPoint);
+
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(startMarker);
+        map.invalidate();
+
+        startMarker.setIcon(ContextCompat.getDrawable(this, R.drawable.marker));
+        startMarker.setTitle("Hello World2");
+    }
+}
